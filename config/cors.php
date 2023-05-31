588fa48da396070ccdc45fdd3ac0b13efc0f5065
leonardoserra <serra.leonardo92@gmail.com>
2023-05-31 15:34:25 +0200

    aggiunto cors config

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [env('APP_FRONTEND_URL', 'https://127.0.0.1:5173')],
 
     'allowed_headers' => ['*'],
 
